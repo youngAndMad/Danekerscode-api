194610131ebd7604a575b1dcf3804c9b1fb6d2c4
youngandmad <kkraken2005@gmail.com>
2023-10-13 00:23:39 +0600

    init

---
+++ src/test/java/danekerscode/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package danekerscode.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
